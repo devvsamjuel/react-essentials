01278c32e616a463a8f211a83374c38c7673f588
samuel <samuel@errandpay.com>
2025-01-05 14:39:54 +0100

    update-images

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import reactAtomImage from "../../assets/react.svg";
+import reactAtomImage from "../../assets/react-core-concepts.png";
 import "./Header.css";
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
