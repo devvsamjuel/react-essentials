fa6b6c330ae1ffb6f66bfdd7029ebc8a23a52815
samuel <samuel@errandpay.com>
2025-01-08 20:43:02 +0100

    clean

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -17,6 +17,5 @@ export default TabButton;
 // Prop validation using PropTypes
 TabButton.propTypes = {
     isSelected: PropTypes.bool.isRequired,
-    text: PropTypes.string.isRequired,
-    onSelect: PropTypes.func.isRequired
+    text: PropTypes.string.isRequired
 };
